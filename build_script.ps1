370f8d4980dbbb561fcea106d41f28f11fe8858c
aragas <personal@aragas.org>
2019-10-07 16:39:13 +0300

    simplified appveyor stuff

---
+++ build_script.ps1
@@ -0,0 +1,4 @@
+msbuild $env:SOLUTION_NAME /verbosity:m /property:Configuration=Windows /property:Platform=x86
+msbuild $env:SOLUTION_NAME /verbosity:m /property:Configuration=Windows /property:Platform=x64
+msbuild $env:SOLUTION_NAME /verbosity:m /verbosity:m /property:Configuration=OSX-Linux /property:Platform=x86
+msbuild $env:SOLUTION_NAME /verbosity:m /verbosity:m /property:Configuration=OSX-Linux /property:Platform=x64
