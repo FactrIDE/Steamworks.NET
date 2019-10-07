8f1e980553df7148662fa67b51a7ff257716eab5
aragas <personal@aragas.org>
2019-10-07 17:06:30 +0300

    nuget deploy was checking if it was in release configuration

--- after_test_nuget.ps1
+++ after_test_nuget.ps1
@@ -1,8 +1,5 @@
-if($env:CONFIGURATION -eq "Release" -and $env:APPVEYOR_REPO_TAG -eq "true" -and $isWindows)
-{
-	cd $env:APPVEYOR_BUILD_FOLDER
-	nuget pack Steamworks.NET-Standalone.nuspec -Version $env:APPVEYOR_BUILD_VERSION
+cd $env:APPVEYOR_BUILD_FOLDER
+nuget pack Steamworks.NET-Standalone.nuspec -Version $env:APPVEYOR_BUILD_VERSION
 	
-	$nupkg = (Get-ChildItem Steamworks.NET-Standalone*.nupkg)[0];
-	Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "Steamworks.NET-Standalone.nupkg";
-}
+$nupkg = (Get-ChildItem Steamworks.NET-Standalone*.nupkg)[0];
+Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "Steamworks.NET-Standalone.nupkg";
